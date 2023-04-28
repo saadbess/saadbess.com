d846c5a21050ad7cb53055984aca193b542a0d21
Saad Koubeissi <saad@koubeissi.com>
2023-04-28 20:12:58 +0400

    feat: added 11ty

---
+++ src/_data/navigation.js
@@ -0,0 +1,30 @@
+module.exports = {
+  top: [
+    {
+      text: 'Home',
+      url: '/'
+    },
+    {
+      text: 'About',
+      url: '/about/'
+    },
+    {
+      text: 'Blog',
+      url: '/blog/'
+    },
+    {
+      text: 'Podcast',
+      url: '/podcast/'
+    }
+  ],
+  bottom: [
+    {
+      text: 'Imprint',
+      url: '/imprint/'
+    },
+    {
+      text: 'Privacy',
+      url: '/privacy/'
+    }
+  ]
+};
